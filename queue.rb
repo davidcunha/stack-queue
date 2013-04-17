376dd0dd54f0f4d6bdaedf6191fc440a1506cb34
David Cunha <davidgoncalvescunha@gmail.com>
2013-04-17 23:21:28 +0100

    stack and queue

---
+++ queue.rb
@@ -0,0 +1,43 @@
+class Queue
+  def initialize(size)
+    @size = size
+    @store = Array.new(@size)
+    @head, @tail = 0, 0
+  end
+
+  def dequeue
+    if empty?
+      nil
+    else
+
+      dequeued = @store[@head]
+      @store[@tail] = nil
+      @tail = @tail - 1
+      dequeued
+    end
+  end
+
+  def enqueue(element)
+    if full? or element.nil?
+      nil
+    else
+      @store[@tail] = element
+      @tail = @tail + 1
+      self
+    end
+  end
+
+  def size
+    @size
+  end
+
+  private
+
+  def empty?
+    @head == @tail
+  end
+
+  def full?
+    @tail == (@size)
+  end
+end
